6367114198920a90af17bcfd6536472768ef0175
debian <debian@debian.debian>
2017-04-13 23:04:01 +0800

    add / update a new learn

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+git add .
+git status
+git commit -am 'add / update a new learn'
+git push origin master
---
+++ yield/readme.md
@@ -0,0 +1,4 @@
+
+## learn from brother niao
+
+http://www.laruence.com/2015/05/28/3038.html
